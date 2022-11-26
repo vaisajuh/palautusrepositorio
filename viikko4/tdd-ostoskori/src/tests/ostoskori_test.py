d0f04b31c8cd02d62de8be19cddc2f344a80e155
vaisajuh <juha.vaisanen@gmail.com>
2022-11-27 00:05:23 +0200

    kahden_eri_lisayksen_jalkeen_kaksi_tavaraa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -20,3 +20,12 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+    
+    def test_kahden_eri_lisayksen_jalkeen_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        maito = Tuote("Leipä", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    
