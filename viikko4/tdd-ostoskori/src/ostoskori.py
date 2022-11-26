48466bdd00fad2cdbd210c357b99437f6a3283b9
vaisajuh <juha.vaisanen@gmail.com>
2022-11-26 20:48:39 +0200

    hinta_ja_maara_alussa

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,16 +3,19 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
+        
+
         pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        pass
+        return 0
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
+
         return 0
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
