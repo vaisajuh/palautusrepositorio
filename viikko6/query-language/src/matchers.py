044758f4fd7db76c81c230d6055017afd8de5aa3
vaisajuh <juha.vaisanen@gmail.com>
2022-12-11 00:28:38 +0200

    teht2

---
+++ viikko6/query-language/src/matchers.py
@@ -0,0 +1,54 @@
+class And:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                return False
+
+        return True
+
+
+class PlaysIn:
+    def __init__(self, team):
+        self._team = team
+
+    def test(self, player):
+        return player.team == self._team
+
+
+class HasAtLeast:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value >= self._value
+
+class All:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+    
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+        return True
+
+class Not:
+    def __init__(self, value):
+        self._value = value
+    
+    def test(self, player):
+        player_value = getattr(player, self._value._attr)
+        return not player_value >= self._value._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+    
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value < self._value
